2bd8718778c7ffeda3724b51718419058df4907d
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2018-02-27 15:09:41 -0500

    updated arduino loop delay

--- accelerometer_example.cpp
+++ accelerometer_example.cpp
@@ -10,10 +10,16 @@ void setup()
   Wire.write(0x6B); // PWR_MGMT_1 register
   Wire.write(0);    // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
+  pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
 }
 void loop()
 {
+  digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+  delay(100);                      // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+
+  // write all info for MPU
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
@@ -25,19 +31,18 @@ void loop()
   GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  Serial.print("AcX = ");
   Serial.print(AcX);
-  Serial.print(" | AcY = ");
+  Serial.print(",");
   Serial.print(AcY);
-  Serial.print(" | AcZ = ");
+  Serial.print(",");
   Serial.print(AcZ);
-  Serial.print(" | Tmp = ");
+  Serial.print(",");
   Serial.print(Tmp / 340.00 + 36.53); //equation for temperature in degrees C from datasheet
-  Serial.print(" | GyX = ");
+  Serial.print(",");
   Serial.print(GyX);
-  Serial.print(" | GyY = ");
+  Serial.print(",");
   Serial.print(GyY);
-  Serial.print(" | GyZ = ");
+  Serial.print(",");
   Serial.println(GyZ);
-  delay(3000);
+  delay(100);
 }
